4006fe761ad148c9e8db1a74f4a8fd6e60ec8599
rapdos <raphael.santos.esteves@gmail.com>
2026-05-15 17:33:23 -0300

    setup

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Quantum-Portfolio-for-Dev/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,8 +3,5 @@
 # script directory
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-echo "[$0] Docker composing up..."
-docker compose -f $SCRIPT_DIR/docker-compose.yml up
-
-echo "[$0] Docker composing down..."
-docker compose -f $SCRIPT_DIR/docker-compose.yml down
+docker compose -f "$SCRIPT_DIR/docker-compose.yml" up
+docker compose -f "$SCRIPT_DIR/docker-compose.yml" down
