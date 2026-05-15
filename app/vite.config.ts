15b8bd7a67fe6a62f81cf4ff5809a036692968bd
rapdos <raphael.santos.esteves@gmail.com>
2026-05-15 17:54:46 -0300

    fix vite base config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Quantum-Portfolio-for-Devs/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
